30c58e4c092603f05411a42fffc869d4efdd7cef
josedomingolopez <josedomingo.lopez@medianet.es>
2013-05-18 12:30:49 +0200

    Optimize.

--- js/regexp-tester.js
+++ js/regexp-tester.js
@@ -1,23 +1,22 @@
 (function(){
   "use strict";
 
-  var emailRe = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  var emailRe = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      validationResult = document.querySelector("#emailValidation"),
+      test = function(stringToTest) {
+        if(emailRe.test(stringToTest)) {
+          validationResult.innerText= "Success";
+          validationResult.classList.remove("label-important");
+          validationResult.classList.add("label-success");
+        } else {
+          validationResult.innerText = "Error";
+          validationResult.classList.remove("label-success");
+          validationResult.classList.add("label-important");
+        }
+      },
+      emailInput = document.querySelector("#email");
 
-  var validationResult = document.querySelector("#emailValidation");
-  var test = function(stringToTest) {
-    if(emailRe.test(stringToTest)) {
-      validationResult.innerText= "Success";
-      validationResult.classList.remove("label-important");
-      validationResult.classList.add("label-success");
-    } else {
-      validationResult.innerText = "Error";
-      validationResult.classList.remove("label-success");
-      validationResult.classList.add("label-important");
-    }
-  };
-
-  var emailInput = document.querySelector("#email");
   emailInput.addEventListener("keyup", function(){
     test(this.value);
-  });
+  }, false);
 }());
